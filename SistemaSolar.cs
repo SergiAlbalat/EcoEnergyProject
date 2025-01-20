f1970a3ab45f14a3b45c413815a2c7f95c39df95
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-01-20 12:37:27 +0100

    Energia solar acabada Closes #1

---
+++ SistemaSolar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace EcoEnergyProject
+{
+    public class SistemaSolar : SistemaEnergia, ICalculEnergia
+    {
+        public SistemaSolar(double horesDeSol)
+        {
+            if(CheckParameter(horesDeSol))
+            {
+                Type = "Sistema Solar";
+                Energy = CalculateEnergy(horesDeSol);
+                Date = DateTime.Now;
+            }
+        }
+        public bool CheckParameter(double horesDeSol) => horesDeSol > 1;
+        public double CalculateEnergy(double horesDeSol) => horesDeSol * 1.5;
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Tipus de Energia\tEnergia Generada:\tData:\n{Type}\t{Energy}\t{Date}");
+        }
+    }
+}
