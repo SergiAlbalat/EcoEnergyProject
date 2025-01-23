a21daa260a65a864993069b9a93075144ba53042
SergiAlbalat <sergi.albalat.7e8@itb.cat>
2025-01-23 13:57:34 +0100

    Toques finales hechos Closes #15

--- SistemaEnergia.cs
+++ SistemaEnergia.cs
@@ -9,8 +9,23 @@ namespace EcoEnergyProject
         public static int ContadorSimulacions = 0;
         protected static double ParametrePerDefecte = 20.0d;
         protected static string ErrorForaRang = "L'argument es troba fora de rang";
+
+        /// <summary>
+        /// Dona el contingut del atribut Tipus
+        /// </summary>
+        /// <returns>Constingut de Tipus</returns>
         public string GetTipus() => Tipus;
+
+        /// <summary>
+        /// Dona el contingut del atribut Energia
+        /// </summary>
+        /// <returns>Contingut de Energia</returns>
         public double GetEnergia() => Energia;
+
+        /// <summary>
+        /// Dona el contingut del atribut Data
+        /// </summary>
+        /// <returns>Contingut de Data</returns>
         public DateTime GetData() => Data;
     }
 }
